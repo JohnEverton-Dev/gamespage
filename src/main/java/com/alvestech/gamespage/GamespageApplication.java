a8d8cef2f33d53727cd31fd38f0e9c80326e4ab3
JohnEverton-Dev <j.everton.ti@gmail.com>
2023-08-02 16:27:17 -0300

    properties, Game, ORM

--- src/main/java/com/alvestech/gamespage/GamespageApplication.java
+++ src/main/java/com/alvestech/gamespage/GamespageApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GamespageApplication {
+public class  GamespageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamespageApplication.class, args);
