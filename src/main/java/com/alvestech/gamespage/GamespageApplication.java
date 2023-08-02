282f5b4c1c5f43f91df375fa0f3b9c3067b43b0d
JohnEverton-Dev <j.everton.ti@gmail.com>
2023-08-02 10:36:16 -0300

    iniciando projeto

---
+++ src/main/java/com/alvestech/gamespage/GamespageApplication.java
@@ -0,0 +1,13 @@
+package com.alvestech.gamespage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamespageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamespageApplication.class, args);
+	}
+
+}
